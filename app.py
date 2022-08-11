270515bcc5449e0ac5a58c7aa221bbad60e7249e
Karen <ka.spk27@gmail.com>
2022-08-11 17:25:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, json, request 
-import logging 
-import datetime
 from cs50 import SQL
-import sqlite3
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -14,5 +11,4 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index(): 
-
     return "<p>Hello, World!</p>"
