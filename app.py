401812e72cfcb524ce8da8bf72942c38afbc34c6
Karen <ka.spk27@gmail.com>
2022-08-11 18:48:20 +0300

    1

--- app.py
+++ app.py
@@ -49,102 +49,15 @@ def index():
     
     listOfPapers = []
    
-    # Получаем тукущую цену всех акций пользователя. Дбавляем в список словарей и передаем 
-    for p in paper:
-        listOfPaper = {}
-        priceLookup = lookup(p["symbol_prt"])["price"]
-        listOfPaper["name"] =  p["name_prt"]
-        listOfPaper["symbol"] = p["symbol_prt"]
-        listOfPaper["shares"] = p["shares_prt"]
-        listOfPaper["price"] = usd(priceLookup)
-        listOfPaper["total"] = usd(priceLookup * listOfPaper["shares"])
-        total = total + (priceLookup * listOfPaper["shares"])
-        listOfPapers.append(listOfPaper)
+   
 
-    return render_template("index.html", total = usd(total + cash), cash = usd(cash), listOfPapers = listOfPapers)
+    return render_template("index.html")
 
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    if request.method == "POST":
-        symbol = request.form.get("symbol").upper()
-        shares = request.form.get("shares")
-        today = datetime.datetime.now()
-
-        # Проверяем ввод пользователя
-        if not symbol: 
-            return apology("Missing symbol", 400)
-        if not shares:
-            return apology("Missing shares", 400)
-        if shares.isdigit():
-            shares = int(shares)
-        else:
-            return apology("Not int shares", 400)
-        if shares < 1:
-            return apology("Shares < 1", 400)
-        
-        # Запрашиваем данные по API
-        respond = lookup(symbol)
-        if respond == None:
-            return apology("Invalid symbol", 400)
-
-        price = respond["price"]
-        sum = price * shares
-        userData = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
-        
-        # Проверяем хватает ли средств на балансе пользователя
-        if userData[0]["cash"] < sum:
-            return apology("CAN'T afford", 400)
-        # Изменяем баланс пользователя после покупки
-        balanceAfter = userData[0]["cash"] - sum
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", balanceAfter , session["user_id"])
-
-        # Сохраняем данные портфолио
-        if len(db.execute("SELECT * FROM portfolio WHERE user_id = ? AND symbol_prt = ?", session["user_id"], symbol)) != 1:
-            db.execute("INSERT INTO portfolio (user_id, symbol_prt, name_prt, shares_prt) VALUES (?, ?, ?, 0)", session["user_id"], symbol, respond["name"])
-
-        # Данные до покупки
-        sharesPrt = db.execute("SELECT * FROM portfolio WHERE user_id = ? AND symbol_prt = ?", session["user_id"], symbol)[0]["shares_prt"]
-        #pricePrt = db.execute("SELECT * FROM portfolio WHERE user_id = ? AND symbol_prt = ?", session["user_id"], symbol)[0]["price_prt"]
-
-
-        # Прибавляем купленные акции и расчитывваем новую среднюю стоимость
-        sharesNew = int(shares + sharesPrt)
-       # priceNew = int((price * shares + pricePrt * sharesPrt) / sharesNew)
-       
-        db.execute("UPDATE portfolio SET shares_prt = ? WHERE user_id = ? AND symbol_prt = ?", sharesNew, session["user_id"], symbol)
-
-        # Добавляем данные в историю
-        db.execute("INSERT INTO history (user_id_hst, symbol_hst, name_hst, shares_hst, price_hst, date,  type) VALUES(?, ?, ?, ?, ?, ?, ' ')", session["user_id"], symbol, respond["name"], shares, usd(price), today)
 
         # create table portfolio (id INTEGER NOT NULL, user_id INTEGER NOT NULL, symbol_prt TEXT NOT NULL, name_prt TEXT, shares_prt INTEGER NOT NULL, PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id));
         # create table history (id INTEGER NOT NULL, user_id_hst INTEGER NOT NULL, symbol_hst TEXT NOT NULL, name_hst TEXT, shares_hst INTEGER NOT NULL, price_hst INTEGER NOT NULL, date TEXT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(user_id_hst) REFERENCES users(id));
-       
-        return redirect("/")
-    else:
-        return render_template("buy.html")
 
-@app.route("/buyIndex", methods = ["POST"])
-@login_required
-def buyIndex():
-    symbol = request.form.get("symbol")
-    return render_template("buyIndex.html", symbol=symbol)
-
-@app.route("/sellIndex", methods = ["POST"])
-@login_required
-def sellIndex():
-    symbol = request.form.get("symbol")
-    portfolio = db.execute("SELECT symbol_prt FROM portfolio WHERE user_id = ?", session["user_id"])
-    return render_template ("sellIndex.html", portfolio = portfolio, symbol=symbol)
-
-
-@app.route("/history")
-@login_required
-def history():
-    # user's history
-    history = db.execute("SELECT * FROM history WHERE user_id_hst = ?", session["user_id"]) 
-    return render_template("history.html", history = history)
-
+ 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -195,21 +108,6 @@ def logout():
     return redirect("/")
 
 
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        if not symbol:
-            return apology("Missing symbol", 400)
-        respond = lookup(symbol)
-        if respond == None:
-            return apology("Invalid symbol", 400)
-
-        return render_template("quoted.html", name = respond["name"], symbol = respond["symbol"], price = usd(respond["price"]))
-
-    else:
-      return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -245,103 +143,6 @@ def register():
         return redirect("/")
     else:
         return render_template("register.html")
-
-@app.route("/addMoney", methods=["GET", "POST"])
-@login_required
-def addMoney():
-    if request.method == "POST":
-        numbers = request.form.get("numbers")
-        if not numbers:
-            return apology("Not sum", 400)
-
-        if numbers.isdigit():
-                numbers = int(numbers)
-        else:
-            return apology("numbers not int", 400)
-        if numbers < 1:
-            return apology("numbers not positive", 400)
-
-        cash = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]["cash"] + numbers
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"])
-        return redirect ("/addMoney")
-    else:
-        balance = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-        return render_template("addMoney.html", balance = usd(balance))    
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        shares = request.form.get("shares")
-        today = datetime.datetime.now()
-
-        # Проверяем ввод
-        if not symbol: 
-            return apology("Missing symbol", 400)
-        if not shares:
-            return apology("Missing shares", 400)
-        if shares.isdigit():
-            shares = int(shares)
-        else:
-            return apology("Shares not int", 400)
-        if shares < 1:
-            return apology("Shares not positive", 400)
-        
-        symbol = symbol.upper()
-        # Запрашиваем занные по API
-        respond = lookup(symbol)
-        if not respond["symbol"]:
-            return apology("Error symbol", 400)
-        
-        # Проверяем количесво акций в порфеле
-        portfolioPapers = db.execute("SELECT shares_prt FROM portfolio WHERE user_id = ? AND symbol_prt like ?", session["user_id"], symbol)[0]["shares_prt"]
-        if portfolioPapers < shares:
-            return apology("too many shares", 400)
-
-        # Вносим изменения в портфель, баланс и историю
-        portfolioPapersAfter = portfolioPapers - shares
-        db.execute("UPDATE portfolio SET shares_prt = ? WHERE user_id = ? AND symbol_prt = ?", portfolioPapersAfter, session["user_id"], symbol)
-
-        cashBefor = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-        cashAfter = cashBefor + respond["price"] * shares
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", cashAfter, session["user_id"])
-
-        db.execute("INSERT INTO history (user_id_hst, symbol_hst, name_hst, shares_hst,  price_hst, date, type) VALUES (?, ?, ?, ?, ?, ?, '-')", session["user_id"], symbol, respond["name"], shares, respond["price"], today)
-
-        # Усли акции больше нет, то удаляем этоту строку
-        if db.execute("SELECT shares_prt FROM portfolio WHERE user_id = ? AND symbol_prt = ?", session["user_id"], symbol)[0]["shares_prt"] == 0:
-            db.execute("DELETE FROM portfolio WHERE user_id = ? AND symbol_prt = ?", session["user_id"], symbol)
-        
-
-        return redirect("/")
-    else:
-        # Передаем сисок акций пользователя
-        portfolio = db.execute("SELECT symbol_prt FROM portfolio WHERE user_id = ?", session["user_id"])
-        return render_template ("sell.html", portfolio = portfolio)
-
-@app.route("/password", methods=["GET", "POST"])
-@login_required
-def password():
-    if request.method == "POST":
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        # Check password
-        if not password or checkPassword(password):
-            return apology("Invalid password", 403)
-        if not password or checkPasswordBadSymbol(password):
-            return apology("Invalid password 2", 403)
-        if not confirmation or password != confirmation:
-            return apology("Invalid confirmation", 403)
-        # Update db
-        db.execute("UPDATE users SET hash = ? WHERE id = ?", session["user_id"], generate_password_hash(password, "pbkdf2:sha256"))
-        text = "Password changed!"
-        return render_template("passwordChanged.html", text = text)
-    else:
-        return render_template("password.html")
-
 
 
 #function check password
