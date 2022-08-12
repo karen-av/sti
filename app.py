ffb60dbe371b203214254f82a876db4471aa1def
Karen <ka.spk27@gmail.com>
2022-08-12 13:34:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import datetime
 from turtle import position
 from cs50 import SQL
-import sqlite3
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -23,11 +21,6 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///sti.db")
-
-# Make sure API key is set
-#if not os.environ.get("API_KEY"):
- #   raise RuntimeError("API_KEY not set")
-
 
 @app.after_request
 def after_request(response):
