6d9fb22e3e81b920d5b59f5502b5013264e77db3
Karen <ka.spk27@gmail.com>
2022-08-15 15:56:58 +0300

    csv reader

--- testsql.py
+++ testsql.py
@@ -3,14 +3,20 @@ from cs50 import SQL
 
 db = SQL("sqlite:///sti.db")
 
-with open('upload_files/111.csv') as f:
-    #print(f.readlines())
-    dr = csv.DictReader(f)
-    for i in dr:
-        print(i['\ufeff'])
-    
-    
+with open('upload_files/users.csv', newline="") as csvfile:
+    spam = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for s in spam:
+        name = (s[0]+' ' +s[1])
+        print(name)
+        
 
+
+    #print(f.readlines())
+    #dr = csv.DictReader(f)
+    #for i in dr:
+     #   print(i['\ufeff'])
+    
+   
 
 
     
